e0e163f59fa488955944ef9e8bab9bca3ef07411
Joel Freeman <joel@joelfreeman.xyz>
2019-11-18 13:48:34 +1300

    Updated main.tf
    Updated main.tf - added lambda function, corrected permissions for s3 so only cloudfront can access - not public and setup cloudfront distribution to trigger lambda function.

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,17 +15,67 @@ provider "aws" {
   alias = "us-east-1"
 }
 
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  
+}
+
 // Create S3 bucket. This is used as hosting for the static website.
 resource "aws_s3_bucket" "site_bucket" {
   bucket = var.bucket_name
-  acl    = "public-read"
-  policy = "${file("policy.json")}"
   force_destroy = true
-
+  
   website {
     index_document = "index.html"
     error_document = "404.html"
   }
+}
+// s3 bucket policy , only allow CloudFront access to it 
+resource "aws_s3_bucket_policy" "site_bucket_policy" {
+  bucket = "${aws_s3_bucket.site_bucket.bucket}"
+  
+  policy = <<POLICY
+  {
+    "Version":"2012-10-17",
+    "Id":"S3_CLOUDFRONT_ONLY_ACCESS",
+    "Statement":[
+      {
+        "Sid":"Grant a CloudFront Origin Identity access to support private content",
+        "Effect":"Allow",
+        "Principal":{"CanonicalUser":"${aws_cloudfront_origin_access_identity.origin_access_identity.s3_canonical_user_id}"},
+        "Action":"s3:GetObject",
+        "Resource":"arn:aws:s3:::${var.bucket_name}/*"
+      }
+    ]
+  }
+  POLICY
+}
+
+resource "aws_iam_role" "lambda_iam_role" {
+  name = "iam_lambda_s3"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "s3_lambda_redirect" {
+  filename = "lambda_function.zip"
+  function_name = "lambda_s3_cloudfront_subdirectory_default_index"
+  role = "${aws_iam_role.lambda_iam_role.arn}"
+  handler = "index.handler"
+  runtime = "nodejs8.10"
 }
 
 
@@ -69,6 +119,10 @@ resource "aws_cloudfront_distribution" "s3_cloudfront_distribution" {
   origin {
     domain_name = "${aws_s3_bucket.site_bucket.bucket_regional_domain_name}"
     origin_id = var.origin_id
+
+    s3_origin_config {
+      origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
+    }
   }
 
   enabled = true
@@ -81,19 +135,27 @@ resource "aws_cloudfront_distribution" "s3_cloudfront_distribution" {
     allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods = ["GET", "HEAD"]
     target_origin_id = var.origin_id
-
+    
     forwarded_values {
-      query_string = false
+      query_string = true
 
       cookies {
         forward = "none"
       }
     }
 
+
     viewer_protocol_policy = "redirect-to-https"
     min_ttl = 0
     default_ttl = 3600
     max_ttl = 86400
+
+    lambda_function_association {
+        event_type = "viewer-request"
+        lambda_arn = "${aws_lambda_function.s3_lambda_redirect.qualified_arn}"
+        include_body = true
+    }
+    
   }
   
   restrictions {
@@ -107,6 +169,8 @@ resource "aws_cloudfront_distribution" "s3_cloudfront_distribution" {
     ssl_support_method = "sni-only"
     cloudfront_default_certificate = false
   }
+
+  
 
   depends_on = ["aws_acm_certificate_validation.ssl_cert_validation", "aws_s3_bucket.site_bucket"]
 }
