f1c677b97ae673100218a0c0be5c83d132325c25
Joel Freeman <joel.freeman@tribe.co.nz>
2019-11-20 13:02:38 +1300

    Fixed lambda function

--- terraform/lambda_function_source/index.js
+++ terraform/lambda_function_source/index.js
@@ -6,26 +6,39 @@ exports.handler = (event, context, callback) => {
 
     // Extract the URI from the request
     var olduri = request.uri;
-
+    var newuri;
     if(olduri.endsWith('/')) {
+
         // Match any '/' that occurs at the end of a URI. Replace it with a default index
         var newuri = olduri.replace(/\/$/, '\/index.html');
+        console.log("Requrest URI: " + request.uri);
+        console.log("Old URI: " + olduri);
+        console.log("New URI: " + newuri);       
+
+        request.uri = newuri; // set uri to new uri
     } else if (olduri.endsWith(".css") || olduri.endsWith(".html")) {
 
-        // append "/" to end of string
-        var newuri = olduri + '/'
+        // Do nothing, only write to log
+        newuri = olduri; // only assigning it so i don't get confused if i look at logs
+        console.log("Requrest URI: " + request.uri);
+        console.log("Old URI: " + olduri);
+        console.log("New URI: " + newuri);
+    } else if (olduri.endsWith(".xml")) {
+
+        // Do nothing, only write to log
+        newuri = olduri; // only assigning it so i don't get confused if i look at logs
+        console.log("Requrest URI: " + request.uri);
+        console.log("Old URI: " + olduri);
+        console.log("New URI: " + newuri);       
+    } else {
+        newuri = olduri + "/"; // appending slash to end of path
+        console.log("Requrest URI: " + request.uri);
+        console.log("Old URI: " + olduri);
+        console.log("New URI: " + newuri);
+
+        request.uri = newuri;  // set uri to new uri
     }
 
-    // Match any '/' that occurs at the end of a URI. Replace it with a default index
-    
-    
-    // Log the URI as received by CloudFront and the new URI to be used to fetch from origin
-    console.log("Old URI: " + olduri);
-    console.log("New URI: " + newuri);
-    
-    // Replace the received URI with the URI that includes the index page
-    request.uri = newuri;
-    
     // Return to CloudFront
     return callback(null, request);
 
