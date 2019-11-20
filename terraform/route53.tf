c79886ce2c57f283c8cc424d9304c223645dc783
Joel Freeman <joel@joelfreeman.xyz>
2019-11-21 09:41:55 +1300

    fixed formatting in route53.tf

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -7,8 +7,8 @@ resource "aws_route53_record" "ssl_cert_dns_validation_records" {
     name        = "${aws_acm_certificate.ssl_cert.domain_validation_options.0.resource_record_name}"
     type        = "${aws_acm_certificate.ssl_cert.domain_validation_options.0.resource_record_type}"
     zone_id     = "${data.aws_route53_zone.hosted_zone.id}"
-	records     = ["${aws_acm_certificate.ssl_cert.domain_validation_options.0.resource_record_value}"]
-	ttl         = 60
+    records     = ["${aws_acm_certificate.ssl_cert.domain_validation_options.0.resource_record_value}"]
+    ttl         = 60
 
     depends_on = ["aws_acm_certificate.ssl_cert"]
 }
